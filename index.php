5b8a4ca382409fad97ae539cbc23f890adc1976f
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-03-31 18:43:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 
 /*
- * Copyright (c) 2018 Barchampas Gerasimos <makindosx@gmail.com>
- * online-banking a online banking system for local businesses.
- *
- * online-banking is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
+ * Copyright (c) 2019 - 2020 AYCH inc <tokeneconomy@eclipso.eu>
+ * File LICENSE or AGPL-3.0 or later, (https://www.gnu.org/license/).
+ * 
+ * The project is an online-banking software as a service (SaaS).
+ * It is under the terms of the GNU Affero General Public License as
+ * published by the AYCH inc Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
  *
  * online-banking is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -38,7 +37,7 @@
  
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></scrohipt>
     
 
 
