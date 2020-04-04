0a69711710af802641115034642eb6013597c1b2
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-04 17:47:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
 
 /*
- * Copyright (c) 2019 - 2020 AYCH inc <tokeneconomy@eclipso.eu>
- * File LICENSE or AGPL-3.0 or later, (https://www.gnu.org/license/).
- * 
- * The is an online-banking software used by AYCHBank and may be delivered as a service (SaaS).
- * It is under the terms of the GNU Affero General Public License as
- * developed by the AYCH inc.
+ * Copyright (c) 2015 - 2020 Jean Wallet
+ * Copyright (c) 2015 - 2020 The AYCHDeveloper
+ * Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+ * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+ * file LICENSE or https://www.gnu.org/licenses.
  *
- * The AYCHBank is used in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * This is an online-banking developed by AYCHDeveloper. It is initially built in the banking system for our local businesses.
+ * AYCHBank is used under the terms of the GNU Affero General Public License version 3.0 or later. When dealing with international registration and transaction, AYCHBank shall follow the IFM regulations and the common bank security requirements. Whenever it is distributed to the third party for use 
+ * it is served as a SaaS provided by AYCH inc with relevant license and regulations. (It is also restricted to redistribute the AYCHBank software. It is not recommended to resided it without a notice permission from AYCH inc.
+ *
+ * AYCHBank is not a game. It is used for online transaction is used in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License, version 3,
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
+ */
  */
 
  session_start();
@@ -29,7 +32,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-       <title> Easy bank </title>
+       <title> AYCHBank </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
  
