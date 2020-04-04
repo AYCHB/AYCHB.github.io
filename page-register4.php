c11fe5c1c439f523c6b516778a7a49281778d98b
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-04 20:41:19 +0700

    Update page-register4.php

--- page-register4.php
+++ page-register4.php
@@ -1,19 +1,18 @@
 <?php
 
 /*
- * Copyright (c) 2018 Barchampas Gerasimos <makindosx@gmail.com>
- * online-banking a online banking system for local businesses.
+ * Copyright (c) 2015 - 2020 Jean Wallet
+ * Copyright (c) 2015 - 2020 The AYCHDeveloper
+ * Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+ * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+ * file LICENSE or https://www.gnu.org/licenses.
  *
- * online-banking is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This is an online-banking developed by AYCHDeveloper. It is initially built in the banking system for our local businesses.
+ * AYCHBank is used under the terms of the GNU Affero General Public License version 3.0 or later. When dealing with international registration and transaction, AYCHBank shall follow the IFM regulations and the common bank security requirements. Whenever it is distributed to the third party for use 
+ * it is served as a SaaS provided by AYCH inc with relevant license and regulations. (It is also restricted to redistribute the AYCHBank software. It is not recommended to resided it without a notice permission from AYCH inc.
  *
- *
- * online-banking is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * AYCHBank is not a game. It is used for online transaction is used in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License, version 3,
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
@@ -49,7 +48,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title> Easy bank </title>
+    <title> AYCHBank </title>
     <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -229,7 +228,7 @@ window.onload=changeHashOnLoad;
                 <div class="login-logo">
                     <a href="index.php">
                       <!--  <img class="align-content" src="images/logo.png" alt=""> -->
-                      <h2 align="center"> <font color="white"> <b> EASY BANK ACCOUNT </b> </font> </h2>
+                      <h2 align="center"> <font color="white"> <b> AYCH BANK ACCOUNT </b> </font> </h2>
                     </a>
                 </div>
                 <div class="login-form"  style="width: 550px; position: relative; left: 0%;">
@@ -483,7 +482,7 @@ if (isset($_POST['submit_end']))
 
 
   $sql3 = "insert into notifications (email, lastname, firstname, title, message)
-           values ('$email', '$last_name', '$first_name', 'Welcome', 'Welcome to Easy Bank');";
+           values ('$email', '$last_name', '$first_name', 'Welcome', 'Welcome to AYCH Bank');";
   $sql3.= "insert into notifications (email, lastname, firstname, title, message)
            values ('$email', '$last_name', '$first_name', 'Balance', 'Your balance is 0.00 Euro');";
   $sql3.= "insert into notifications (email, lastname, firstname, title, message)
@@ -536,7 +535,7 @@ $mail->Password = '';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 
-$mail->setFrom('Easybank', 'Easybank');
+$mail->setFrom('AYCHBank', 'AYCHBank');
 
 // Add a recipient
 $mail->addAddress($email);
