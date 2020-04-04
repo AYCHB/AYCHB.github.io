f1ee0ec19b50cc0a7c006b2818424dae6a206f0b
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-04 19:37:25 +0700

    Update and rename transf_easy_bank_i_code.php to transf_aych_bank_i_code.php

--- transf_aych_bank_i_code.php
+++ transf_aych_bank_i_code.php
@@ -1,19 +1,18 @@
 <?php
 
 /*
- * Copyright (c) 2018 Barchampas Gerasimos <makindosx@gmail.com>
- * online-banking a online banking system for local businesses.
+ * Copyright (c) 2015 - 2020 Jean Wallet
+ * Copyright (c) 2015 - 2020 The AYCHDeveloper
+ * Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
+ * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+ * file LICENSE or https://www.gnu.org/licenses.
  *
- * online-banking is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This is an online-banking developed by AYCHDeveloper. It is initially built in the banking system for our local businesses.
+ * AYCHBank is used under the terms of the GNU Affero General Public License version 3.0 or later. When dealing with international registration and transaction, AYCHBank shall follow the IFM regulations and the common bank security requirements. Whenever it is distributed to the third party for use 
+ * it is served as a SaaS provided by AYCH inc with relevant license and regulations. (It is also restricted to redistribute the AYCHBank software. It is not recommended to resided it without a notice permission from AYCH inc.
  *
- *
- * online-banking is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * AYCHBank is not a game. It is used for online transaction is used in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License, version 3,
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
@@ -45,7 +44,7 @@ if (time()-$_SESSION['timestamp']>$idletime)
     $_SESSION['timestamp']=time();
      }
 
- if (isset($_POST['transfer_easy_bank'])) 
+ if (isset($_POST['transfer_aych_bank'])) 
       {
 
 
@@ -111,7 +110,7 @@ if (time()-$_SESSION['timestamp']>$idletime)
                              //  exit;
                             echo '<script type="text/javascript">alert("i_code error. Please try again.");
                             </script>';
-                           echo ("<script>location.href='transf_easy_bank.php'</script>");
+                           echo ("<script>location.href='transf_aych_bank.php'</script>");
                                }
                               
 
@@ -137,7 +136,7 @@ if (time()-$_SESSION['timestamp']>$idletime)
 
                      echo '<script type="text/javascript">alert("Verification code expired  Get new code to finish transferring.");
                          </script>';
-                       echo ("<script>location.href='transf_easy_bank.php'</script>");
+                       echo ("<script>location.href='transf_aych_bank.php'</script>");
 
                              }
 
@@ -148,7 +147,7 @@ if (time()-$_SESSION['timestamp']>$idletime)
 
                       // echo '<script type="text/javascript">alert("i_code is ok");
                        //  </script>';
-                      // echo ("<script>location.href='transf_easy_bank.php'</script>");
+                      // echo ("<script>location.href='transf_aych_bank.php'</script>");
 
                             } 
 
