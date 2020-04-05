55b4edc4a262e94c296e64db98caa61e29cf8b57
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-05 09:11:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2015 - 2020 Jean Wallet
+ * Copyright (c) 2015 - 2020 Jean Wallet.
  * Copyright (c) 2015 - 2020 The AYCHDeveloper
  * Distributed under the MIT software license, the AGPL-3.0 or later, see the accompanying
  * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
